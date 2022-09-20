4baed5b91c89e6adc79d11742e20b082cecb3ea9
MattW <mattnwarnock@yahoo.co.uk>
2022-09-20 10:35:04 +0100

    Feature test build

--- src/main/java/ATM.java
+++ src/main/java/ATM.java
@@ -1,2 +1,17 @@
 public class ATM {
+    private BankConsole console;
+
+    public ATM(BankConsole console) {
+
+        this.console = console;
+    }
+
+    public void deposit(int i) {
+    }
+
+    public void withdraw(int i) {
+    }
+
+    public void printStatment() {
+    }
 }
--- src/test/java/BankFeature.java
+++ src/test/java/BankFeature.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class BankFeature {
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InOrder;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.*;
+
+public class BankFeature {
+    @ExtendWith(MockitoExtension.class)
+
+    @Mock BankConsole console;
+
+    @Test
+    void prints_all_transactions() {
+        ATM atm = new ATM(console);
+
+        atm.deposit(1000);
+        atm.deposit(2000);
+        atm.withdraw(500);
+        atm.printStatment();
+
+        InOrder inOrder = inOrder(console);
+
+        inOrder.verify(console).printLine("date || credit || debit || balance");
+        inOrder.verify(console).printLine("14/01/2012 || || 500.00 || 2500.00");
+        inOrder.verify(console).printLine("13/01/2012 || 2000.00 || || 3000.00");
+        inOrder.verify(console).printLine("10/01/2012 || 1000.00 || || 1000.00");
+    }
 }
